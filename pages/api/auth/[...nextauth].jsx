2726fa68a977b44eab3612105db11f83525a631b
Aryan Kumar <aryanak9163@gmail.com>
2023-11-17 01:14:07 +0530

    added facebook and google login

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions = {
   providers: [
@@ -7,7 +9,51 @@ export const authOptions = {
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          access_type: "offline",
+          prompt: "consent",
+          include_granted_scopes: true,
+        },
+      },
+    }),
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET,
+    }),
   ],
+  pages: {
+    signIn: "/login",
+    signOut: "/",
+    error: "/login",
+    verifyRequest: "/login",
+    newUser: "/",
+  },
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  // callbacks: {
+  //   async jwt(token, user, account, profile, isNewUser) {
+  //     if (account?.accessToken) {
+  //       token.accessToken = account.accessToken;
+  //     }
+  //     return token;
+  //   },
+  //   async session(session, token) {
+  //     session.accessToken = token.accessToken;
+  //     return session;
+  //   },
+  // },
+  // database: process.env.NEXT_PUBLIC_MONOGO_URI,
+  // secret: process.env.NEXT_PUBLIC_SECRET,
+  // jwt: {
+  //   secret: process.env.NEXT_PUBLIC_JWT_SECRET,
+  // },
+  debug: true,
 };
 
 export default NextAuth(authOptions);
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,18 @@
 import { Inter } from "next/font/google";
-import React from "react";
+import React, { useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { getSession } from "next-auth/react";
 const inter = Inter({ subsets: ["latin"] });
+import styles from "@/styles/Register.module.css";
+import Link from "next/link";
+import { ImGithub } from "react-icons/im";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
 const Login = () => {
+  const [form, setForm] = useState({});
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {};
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
@@ -12,17 +20,87 @@ const Login = () => {
       <div className="text-5xl font-bold">
         <h1>Login</h1>
       </div>
-      <button
-        onClick={() =>
-          signIn("github", {
-            callbackUrl: "http://localhost:3000/dashboard",
-            redirect: true,
-          })
-        }
-        className="border shadow-md rounded-md p-4 my-4 hover:text-white hover:bg-black hover:shadow-blue-800"
-      >
-        Login with Github
-      </button>
+      <section className={`${styles.form} my-10 w-screen`}>
+        <form onSubmit={handleSubmit} method="POST">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email"
+            required
+            autoComplete="off"
+            onChange={handleChange}
+            value={form.email}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Your Password"
+            required
+            autoComplete="off"
+            onChange={handleChange}
+            value={form.password}
+            className=""
+            style={{
+              background: "url(/icons/eye.svg) no-repeat right 1rem center",
+              backgroundSize: "1.5rem",
+            }}
+          />
+
+          <input type="submit" value="Register" />
+        </form>
+        <div className={`${styles.consent}`}>
+          <span>New to plateform?</span>
+          <Link
+            href="/register"
+            className="text-center text-blue-500 underline"
+          >
+            Register
+          </Link>
+        </div>{" "}
+        <hr className=" border-black border w-[90%] text-center mx-auto" />
+      </section>
+      <section className="oauth text-center">
+        <p>
+          <span className="capitalize font-bold text-xl">continue With</span>
+        </p>
+        <div className="flex justify-center">
+          <button
+            onClick={() =>
+              signIn("github", {
+                callbackUrl: "http://localhost:3000/dashboard",
+                redirect: true,
+              })
+            }
+            className="shadow-md m-4 rounded-lg p-4 my-4 hover:text-white hover:bg-black hover:shadow-gray-900 "
+          >
+            <ImGithub size={30} />
+          </button>{" "}
+          <button
+            onClick={() =>
+              signIn("facebook", {
+                callbackUrl: "http://localhost:3000/dashboard",
+                redirect: true,
+              })
+            }
+            className="shadow-md m-4 rounded-lg p-4 my-4 text-[#0866ff] hover:text-white hover:bg-[#0866ff] hover:shadow-gray-900"
+          >
+            <FaFacebook size={30} />
+          </button>
+          <button
+            onClick={() =>
+              signIn("google", {
+                callbackUrl: "http://localhost:3000/dashboard",
+                redirect: true,
+              })
+            }
+            className="shadow-md m-4 rounded-lg p-4 my-4 hover:text-white hover:bg-black hover:shadow-gray-900"
+          >
+            <FcGoogle size={30} />
+          </button>
+        </div>
+      </section>
     </main>
   );
 };
